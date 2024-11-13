bf9449a7297f14b2d8023b17f7b7c035f3df876f
Кривдин Артем <akrivdin@data-platform.ru>
2024-11-13 12:33:48 +0300

    minor fix

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,12 +1,10 @@
 import UIKit
-import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         window = UIWindow()
         window?.rootViewController = TabBarViewController()
         window?.makeKeyAndVisible()
